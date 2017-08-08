1d2d9518182fac736ab1485c2b91826ceee61f89
Marlon O'Neil <hello@marlononeil.com>
2017-08-08 15:42:17 -0400

    completed exercise 6.1

--- exercise5.rb
+++ exercise5.rb
@@ -7,23 +7,33 @@ distance_from_home = 0
 
 
 while distance_from_home >= 0
-    puts "What action would you like to take? Walk or Run?"
+    puts "What action would you like to take? Walk, Run or Go Home?"
     user_action = gets.chomp.downcase
-
-
-    until user_action == "walk" || user_action == "run"
-      puts "I'm sorry, I only understand 'Walk' or 'Run'."
-      print "Please enter your action again: "
-      user_action = gets.chomp.downcase
-    end
-
-    #If they walk, the total distance should go up by one, and you should update the user on their total distance traveled as follows:
-
-    if user_action == "walk"
-      distance_from_home += 1
+    if user_action == "go home"
+      puts "Distance from home is #{distance_from_home}km"
+      puts "Done exercising already? See ya!"
+      break
     else
-      distance_from_home+= 5
-    end
 
-    puts "Distance from home is #{distance_from_home}km"
+      until user_action == "walk" || user_action == "run" || user_action == "go home"
+        puts "I'm sorry, I only understand 'Walk', 'Run' or 'Go Home'."
+        print "Please enter your action again: "
+        user_action = gets.chomp.downcase
+      end
+
+      #If they walk, the total distance should go up by one, and you should update the user on their total distance traveled as follows:
+
+      if user_action == "go home"
+        puts "Distance from home is #{distance_from_home}km"
+        puts "Done exercising already? See ya!"
+        break
+      elsif user_action == "walk"
+        distance_from_home += 1
+      else
+        distance_from_home+= 5
+      end
+
+      puts "Distance from home is #{distance_from_home}km"
+end
+
 end
